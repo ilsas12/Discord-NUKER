a43b5c38cd31bcabcab02c61e21984703d86cf55
ilsas12 <137562678+ilsas12@users.noreply.github.com>
2023-06-27 17:05:19 +0200

    Add files via upload

---
+++ Discord NUKER/start.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo "Je ne suis absolument pas responsable de vos acte et de ce qui vous arrive"
+
+
+set /p answer="Do you want to continue (Y/N)? "
+
+if /i "%answer%"=="Y" (
+  echo "You chose to continue."
+) else if /i "%answer%"=="N" (
+  echo "You chose to cancel."
+) else (
+  echo "Invalid input."
+)
+
+timeout /t 5 /nobreak
+
+python menu.py
